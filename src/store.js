4e69a1429db0af5253201cb72b173c9b1736fd93
rashmina34 <rashminashrestha@gmail.com>
2018-12-13 13:58:21 +0545

    store for the app

---
+++ src/store.js
@@ -0,0 +1,10 @@
+ import { createStore, applyMiddleware} from 'redux';
+ import thunk from 'redux-thunk';
+ import logger from 'redux-logger';
+ import reducer from './SignIn/reducers';
+
+
+ const store = createStore(reducer, {}, applyMiddleware(thunk, logger));
+ export default store;
+
+
